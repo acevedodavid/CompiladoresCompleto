e35f2afb3b6c4420cb0f4302e3cde01edc837849
ACEVEDD <david.acevedo_villarreal@daimler.com>
2020-11-24 01:41:48 -0600

    Muestra el resultado de la compilación en Node.js
    Aún falta que muestre los cuadruplos y las tablas de simbolos y constantes

--- server.js
+++ server.js
@@ -20,6 +20,8 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 app.use(bodyParser.json());
+//app.use(bodyParser.text({ type: "text/plain" }));
+//var jsonParser = bodyParser.json();
 
 // Server listen
 const port = process.env.PORT || 3000;
@@ -39,36 +41,68 @@ app.get('/blockly.html', (req, res) => {
     res.render('blockly');
 });
 
+app.get('/result.html', (req, res) => {
+    res.render('result');
+});
+
+app.get('/retrieveResults', (req,res) => {
+    console.log("/retrieveResults");
+    var result = "";
+    fs.readFile('output_vm.txt', 'utf8', (err,data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        result = data;
+        console.log(result);
+        res.end(result);
+    });
+});
+
 app.post('/compileCode', (req, res) => {
+    console.log(req.body.code);
+    saveCode(runMain, req.body.code);
+    //res.end();
+});
+
+
+function writeFile(fileName, dataToSend) {
+    fs.writeFile(fileName, dataToSend, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("The file" + fileName + "was saved!")
+    });
+}
+
+function saveCode(callback, reqBodyCode) {
     //save code
     console.log("/saveCode")
     //console.log("saveCode");
     //console.log(req.body);
     //console.log(res.body);
-    fs.writeFile("output_web.txt", req.body.code, function (err) {
+    fs.writeFile("output_web.txt", reqBodyCode, function (err) {
         if (err) {
             return console.log(err);
         }
         console.log("The file output_web.txt was saved!")
+        callback(getOutput);
     });
-    
+}
+
+function runMain(callback) {
     // run main.py
     console.log("/runMain");
     var dataToSend;
     // spawn new child process to call the python script
-    const python = spawn('python3', ['compiler/main.py', 'output_web.txt']);
+    var python = spawn('python3', ['compiler/main.py', 'output_web.txt']);
 
     // collect data from script
     python.stdout.on('data', function (data) {
         console.log('Pipe data from python script ...');
         dataToSend = data.toString();
         //console.log(dataToSend);
-        fs.writeFile("output_main.txt", dataToSend, function (err) {
-            if (err) {
-                return console.log(err);
-            }
-            console.log("The file output_main.txt was saved!")
-        });
+        writeFile("output_main.txt",dataToSend);
     });
 
     // in close event we are sure that stream from child process is closed
@@ -76,7 +110,32 @@ app.post('/compileCode', (req, res) => {
         console.log(`child process close all stdio with code ${code}`);
         // send data to browser
         //res.send(dataToSend)
+        callback();
     });
-    
+}
+
+function getOutput() {
+    // getOutput.py
+    console.log("/getOutput");
+    var dataToSend;
+    // spawn new child process to call the python script
+    const python2 = spawn('python3', ['compiler/virtualMachine.py', 'output_main.txt']);
+
+    //writeFile("output_vm.txt","");
+    // collect data from script
+    python2.stdout.on('data', function (data) {
+        console.log('Pipe data from vm ...');
+        dataToSend = data.toString();
+        //console.log(dataToSend);
+        writeFile("output_vm.txt", dataToSend);
+    });
+
+    // in close event we are sure that stream from child process is closed
+    python2.on('close', (code) => {
+        console.log(`child process close all stdio with code ${code}`);
+        // send data to browser
+        //res.send(dataToSend)
+    });
+
     console.log("Finish method, going out, bye bye");
-});
+}
